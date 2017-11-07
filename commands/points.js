a6e95197809375e167dc5d5f0e5c4aae1df0de90
Glitch (aclipse) <none>
2017-11-07 18:34:06 +0000

    Checkpoint

--- commands/points.js
+++ commands/points.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-  let scorePoints = client.points.get(message.author.id).points;
-  let scoreLevel = client.points.get(message.author.id).level;
+  let user = message.mentions.users.first();
+  if (message.mentions.users.size < 1) user = message.author;
+  let scorePoints = client.points.get(user.id).points;
+  let scoreLevel = client.points.get(user.id).level;
+  if (!scorePoints) {
+    scorePoints = "0";
+    scoreLevel = "0";
+  }
   const settings = client.settings.get(message.guild.id);
   const pointsEmbed = new Discord.RichEmbed()
     .setColor(settings.embedColour)
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,17 +1,17 @@
 module.exports = (client, member) => {
   const settings = client.settings.get(member.guild.id);
   if (settings.welcomeEnabled === "true") {
-    let welcomeMessage = settings.welcomeMessage
+    let welcomeMessage = settings.welcomeMessage;
     welcomeMessage = welcomeMessage.replace("{{user}}", member.user.tag);
     welcomeMessage = welcomeMessage.replace("{{guild}}", member.guild.name);
     welcomeMessage = welcomeMessage.replace("{{memberCount}}", member.guild.memberCount);
     let welcomeChannel = member.guild.channels.find("name", settings.defaultChannel);
-    const defaultChannel = member.guild.channels.find(c=> c.permissionsFor(member.guild.me).has("SEND_MESSAGES"));
-    if (!welcomeChannel) return 
+    if (!welcomeChannel) return member.guild.channels.find(c=> c.permissionsFor(member.guild.me).has("SEND_MESSAGES")).send(`:negative_squared_cross_mark: ${settings.defaultChannel} was not found`);
     member.guild.channels.find("name", settings.defaultChannel).send(welcomeMessage);
   }
   if (settings.autoRole === "true") {
     let role = member.guild.roles.find("name", settings.autoRole);
+    if (!role) return;
     member.addRole(role);
   }
 };
