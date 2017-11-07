e60f79ce3ba516223038f6a36295a972016b7efe
Glitch (aclipse) <none>
2017-11-07 18:01:07 +0000

    Checkpoint

--- commands/points.js
+++ commands/points.js
@@ -1,6 +1,9 @@
 exports.run = (client, message, args) => {
-  const scorePoints = client.points.get(message.author.id).points;
-  const scoreLevel = client.points.get(message.author.id).level;
+  let user;
+  if (message.mentions.users.size === 0) user = message.author;
+  if (message.mentions.users.size === 1) user = message.mentions.users.first();
+  const scorePoints = client.points.get(user.id).points;
+  const scoreLevel = client.points.get(user.id).level;
   !scorePoints ? message.channel.send('You have no points yet.') : message.channel.send(`You are level ${scoreLevel} with ${scorePoints} points!`);
 };
 
--- commands/reload.js
+++ commands/reload.js
@@ -1,11 +1,11 @@
 exports.run = async (client, message, args) => {
   if (message.author.id !== client.config.ownerId) return;
-  if (!args || args.size < 1) return message.reply('"':negative_squared_cross_mark:Must provide a command to reload.");
+  if (!args || args.size < 1) return message.channel.send(':negative_squared_cross_mark: Must provide a command to reload.');
   let response = await client.unloadCommand(args[0]);
-  if (response) return message.reply(`Error Unloading: ${response}`);
+  if (response) return message.channel.send(`:negative_squared_cross_mark: Error Unloading: ${response}`);
   response = client.loadCommand(args[0]);
-  if (response) return message.reply(`Error Loading: ${response}`);
-  message.reply(`The command \`${args[0]}\` has been reloaded`);
+  if (response) return message.channel.send(`:negative_squared_cross_mark: Error Loading: ${response}`);
+  message.channel.send(`:white_check_mark: The command \`${args[0]}\` has been reloaded`);
 };
 
 exports.cmdConfig = {
