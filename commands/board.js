5b80359d4968a9cc0a44e482f2bf27610b5083fd
Glitch (aclipse) <none>
2017-11-08 17:11:02 +0000

    Checkpoint

--- commands/board.js
+++ commands/board.js
@@ -4,8 +4,10 @@ exports.run = (client, message, [search, ...note]) => {
   const settings = client.settings.get(message.guild.id);
   const board = client.board.get(message.guild.id);
   if (search === "view") {
+    if (!board) return message.channel.send(':negative_squared_cross_mark: You have no notes!')
     const boardEmbed = new Discord.RichEmbed()
-      .setColor(settings.embedColour);
+      .setColor(settings.embedColour)
+      .addField(`${message.guild.name}'s Message Board`, );
     message.channel.send({embed: boardEmbed});
   } else if (search === "add") {
     
