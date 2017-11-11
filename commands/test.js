b7fce7ecbcc051d5b9db2dd14608f77e6fa434d7
Glitch (aclipse) <none>
2017-11-11 23:13:52 +0000

    Checkpoint

--- commands/test.js
+++ commands/test.js
@@ -1,23 +1,5 @@
-var Canvas = require('canvas')
-  , Image = Canvas.Image
-  , canvas = new Canvas(200, 200)
-  , ctx = canvas.getContext('2d');
-var fs = require('fs');
-var path = require('path');
-
 exports.run = (client, message, args) => {
-  ctx.font = '30px Impact';
-  ctx.rotate(.1);
-  ctx.fillText("Awesome!", 50, 100);
-
-  var te = ctx.measureText('Awesome!');
-  ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-  ctx.beginPath();
-  ctx.lineTo(50, 102);
-  ctx.lineTo(50 + te.width, 102);
-  ctx.stroke();
-  console.log(`${canvas.toDataURL()}`);
-  const pngStream = canvas.createPNGStream().pipe(fs.createWriteStream('export.png'))
+  
 }
 
 exports.cmdConfig = {
