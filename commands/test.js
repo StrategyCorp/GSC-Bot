96ec1a6667425ff1148d785bbbd92b6fcb6b42ee
Glitch (aclipse) <none>
2017-11-11 23:34:52 +0000

    Checkpoint

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,13 @@
 const Discord = require('discord.js');
 const request = require('request');
+const crypto = require('crypto');
 
 exports.run = (client, message, args) => {
   const devID = process.env.SMITEDEVID;
   const authKey = process.env.SMITEAUTHKEY;
+  let method = 'getplayerstatus';
+  let signature;
+  let url = `http://api.smitegame.com/smiteapi.svc/${method}json/${devID}/`;
 }
 
 exports.cmdConfig = {
