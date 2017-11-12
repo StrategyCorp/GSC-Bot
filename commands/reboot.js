b21f6c68706c81fd3d23eb9a3e7d7fe964a2bd71
Glitch (aclipse) <none>
2017-11-12 00:18:52 +0000

    Checkpoint

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,24 +1,9 @@
-exports.run = (client, message) => {
-  if (message.author.id !== client.config.ownerId) return;
-  const collector = message.channel.createCollector(msg => msg.author === message.author, {
-    time: 10000
+exports.run = async (client, message) => {
+  await message.channel.send(":white_check_mark: Bot is shutting down.");
+  client.commands.forEach( async cmd => {
+    await client.unloadCommand(cmd);
   });
-  message.channel.send('are you sure?');
-  collector.on('message', msg => {
-    if (msg.content === 'no') collector.stop('aborted');
-    if (msg.content === 'yes') collector.stop('success');
-  });
-  collector.on('end', (collected, reason) => {
-    if (reason === 'time') return message.channel.send('The prompt timed out...');
-    if (reason === 'aborted') return message.channel.send('The reboot has been aborted');
-    if (reason === 'success') {
-      message.channel.send('Rebooting...').then(() => {
-        process.exit();
-      }).catch(e => {
-        console.error(e);
-      });
-    }
-  });
+  process.exit(1);
 };
 
 exports.cmdConfig = {
--- commands/test.js
+++ commands/test.js
@@ -10,12 +10,15 @@ exports.run = (client, message, args) => {
   let method = 'getplayerstatus';
   let timestamp = moment().format('yyyyMMddHHmmss')
   let signature = `${devID}createsession${authKey}${timestamp}`;
-  let createsession = `http://api.smitegame.com/smiteapi.svc/createsessionJson/${devID}/${authKey}`;
+  let createsession = `http://api.smitegame.com/smiteapi.svc/createsessionJson/${devID}/${signature}/${timestamp}`;
   let url = `http://api.smitegame.com/smiteapi.svc/${method}json/${devID}/${signature}/${timestamp}/${playerName}`;
-  request(signature, function(error, response, body){
-    
+  request(createsession, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(JSON.parse(body));
+    } else {
+      console.log(response.statusCode);
+    }
   })
-  }
 }
 
 exports.cmdConfig = {
