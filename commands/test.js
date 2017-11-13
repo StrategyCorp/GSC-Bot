57bb6b9917b3e39bded6a7eb494bc62e2dfcc161
Glitch (aclipse) <none>
2017-11-13 02:03:53 +0000

    Checkpoint

--- commands/test.js
+++ commands/test.js
@@ -8,17 +8,18 @@ exports.run = (client, message, args) => {
   const devID = process.env.SMITEDEVID;
   const authKey = process.env.SMITEAUTHKEY;
   let method = 'getplayerstatus';
-  let timestamp = moment().format('yyyyMMddHHmmss')
+  let timestamp = moment().format('yyyyMMddHHmmss');
   let signature = `${devID}createsession${authKey}${timestamp}`;
   let createsession = `http://api.smitegame.com/smiteapi.svc/createsessionJson/${devID}/${signature}/${timestamp}`;
   let url = `http://api.smitegame.com/smiteapi.svc/${method}json/${devID}/${signature}/${timestamp}/${playerName}`;
-  request(createsession, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log(JSON.parse(body));
-    } else {
-      console.log(response.statusCode);
-    }
-  })
+  console.log(timestamp);
+  // request(createsession, function(error, response, body) {
+  //   if (!error && response.statusCode === 200) {
+  //     console.log(JSON.parse(body));
+  //   } else {
+  //     console.log(response.statusCode);
+  //   }
+  // })
 }
 
 exports.cmdConfig = {
