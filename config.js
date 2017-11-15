ef71e47da6302e9308baf0f9571bf1f93cd0a3de
Glitch (aclipse) <none>
2017-11-15 22:00:25 +0000

    Checkpoint

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const config = {
   "ownerId": "166641492113358848",
-  "games": ["SMITE", "with your mind", "nothing", "with fire", "with some cool wristbands"],
+  "games": ["SMITE", "with your mind", "nothing", "with fire"],
   "gameChangeTime": "15",
   "musicPasses": "1",
   "serverSettings": {
     "prefix": "!",
     "adminRole": "admin",
-    "modlogChannel": "mod-log",
-    "modlogEnable": "true",
     "defaultChannel": "general",
     "welcomeMessage": "Welcome {{user}} to {{guild}}. Member count is now {{memberCount}}",
-    "welcomeEnable": "true",
+    "welcomeEnable": "false",
     "leaveMessage": "{{user}} has left {{guild}}. Member count is now {{memberCount}}",
     "leaveEnable": "false",
     "autoRole": "member",
     "autoEnable": "false",
+    "modlogChannel": "mod-log",
+    "modlogEnable": "false",
     "embedColour": "#8F00FF"
   }
 }
--- events/ready.js
+++ events/ready.js
@@ -2,12 +2,11 @@ module.exports = async client => {
   await client.wait(1000);
   client.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`);
   client.guilds.filter(g => !client.settings.has(g.id)).forEach(g => client.settings.set(g.id, client.config.serverSettings));
-  let game = client.config.games;
-  game = game[Math.floor(Math.random() * game.length)];
-  client.user.setGame(game);
   let minutes = client.config.gameChangeTime;
   let interval = minutes * 60 * 1000;
   setInterval(function() {
+    let game = client.config.games;
+    game = game[Math.floor(Math.random() * game.length)];
     console.log(`Game changed to: ${game}`);
     client.user.setGame(game);
   }, interval);
