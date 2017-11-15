b5759ad07b88558440af5e8ac48fb519de042c6c
Glitch (aclipse) <none>
2017-11-15 02:20:39 +0000

    Checkpoint

--- commands/joke.js
+++ commands/joke.js
@@ -3,22 +3,30 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
   const settings = client.settings.get(message.guild.id);
   let jokeList = {
+    "SexyRexsi's Winrate": ". . .",
     "Why does everyone think Bacchus is so annoying?": "Because he's always whining",
     "What is Sol's favourite movie?": "Twilight: Breaking Down",
-    "Why does everyone think that Xing Tian uses drugs?": "Because he's always Xing things."
+    "Why does everyone think that Xing Tian uses drugs?": "Because he's always Xing things",
+    "What type of camera does Vulcan use": "A cannon",
+    "Why does Medusa make the best weed dealer": "She loves to help people get stoned",
+    "Did you hear He Bo used to be a celebrity": "Now he's all washed up",
+    "I told a joke about Awilix to my friend,": "but it went over his head",
+    "Why isn't Sobek ever in a serious relationship?": "Because all he's ever looking for is a fling"
   };
   let jokeArray = Object.keys(jokeList);
-  let jokeNumber = parseInt(args);
-  jokeNumber--;
-  if (jokeArray[jokeNumber] !== undefined) {
-    var joke = jokeArray[jokeNumber];
+  let forceNumber = parseInt(args);
+  forceNumber--;
+  if (jokeArray[forceNumber] !== undefined) {
+    var joke = jokeArray[forceNumber];
   } else {
     var joke = jokeArray[Math.floor(Math.random() * jokeArray.length)];
   }
+  let jokeNumber = jokeArray.indexOf(joke);
+  jokeNumber++;
   const jokeEmbed = new Discord.RichEmbed()
     .setColor(settings.embedColour)
     .addField(`:regional_indicator_q: ${joke}`, `:regional_indicator_a: ${jokeList[joke]}`)
-    .setFooter(`#${jokeList[joke]}`);
+    .setFooter(`#${jokeNumber}`);
   return message.channel.send({embed: jokeEmbed});
 };
 
