7e869127989f7bf84fff05d896a233b3181aa828
Glitch (aclipse) <none>
2017-11-15 00:30:42 +0000

    Checkpoint

--- commands/joke.js
+++ commands/joke.js
@@ -1,7 +1,23 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-  
+  const settings = client.settings.get(message.guild.id);
+  let jokeList = {
+    "EA": "1",
+    "test": "2",
+    "test 2": "3"
+  };
+  const jokeEmbed = new Discord.RichEmbed()
+    .setColor(settings.embedColour);
+  if (!args) {
+    let jokeArray = Object.keys(jokeList);
+    let joke = jokeArray[Math.floor(Math.random() * jokeArray.length)];
+    jokeEmbed.addField(`${joke}`, `${}`)
+  } else if (args !== args) {
+    
+  } else {
+    
+  }
 };
 
 exports.cmdConfig = {
