cb8c7057757b83d8c8cba33f484273430680af00
Glitch (aclipse) <none>
2017-11-15 21:49:25 +0000

    Checkpoint

--- events/ready.js
+++ events/ready.js
@@ -18,7 +18,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
+  //console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
 });
 
