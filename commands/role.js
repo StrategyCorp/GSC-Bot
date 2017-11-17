56fa23d4032c5a04977957a37c43894bc3bc04e7
Glitch (aclipse) <none>
2017-11-17 07:25:54 +0000

    Checkpoint

--- commands/role.js
+++ commands/role.js
@@ -4,6 +4,7 @@ exports.run = (client, message, args) => {
   let manageMessages = message.member.hasPermission("MANAGE_ROLES");
   if (manageMessages === false) return message.channel.send(':negative_squared_cross_mark: You do not have permission. You need \`MANAGE_ROLES\`');
   let user = message.mentions.users.first();
+  let usergm = message.guild.member(message.mentions.users.first());
   if (message.mentions.users.size < 1) {
     let search = args[0];
     console.log("args");
@@ -13,8 +14,8 @@ exports.run = (client, message, args) => {
     let role = message.guild.roles.find("name", roleName);
     if (!role) return message.channel.send(`:negative_squared_cross_mark: \`${roleName}\` isn't a role`);
     if (message.member.highestRole.comparePositionTo(role) < 1) return message.channel.send(':negative_squared_cross_mark: You cannot give somebody a role higher or equal to your own');
-    console.log(message.member.highestRole.comparePositionTo(role));
-    //user.addRole(role);
+    usergm.addRole(role);
+    return message.channel.send(`:white_check_mark: ${user.username} has been given ${role.name}`);
   }
 };
 
