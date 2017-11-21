c5a84802f27328de5e35e7cfc8a93cd44b1c9373
Glitch (aclipse) <none>
2017-11-21 13:17:29 +0000

    Checkpoint

--- commands/ratemywaifu.js
+++ commands/ratemywaifu.js
@@ -1,20 +1,16 @@
 const Discord = require('discord.js');
-const giSearch = require('google-image-search');
-const fs = require('fs');
 
-exports.run = async (client, message, waifu) => {
+exports.run = (client, message, waifu) => {
   if (!waifu[0]) return message.channel.send(':negative_squared_cross_mark: You must tell me who your waifu is');
   waifu = waifu.join(' ');
   waifu = waifu.toLowerCase();
   const settings = client.settings.get(message.guild.id);
   let score = client.randomNum(1, 10);
   if (waifu === "blake" || waifu === "blake belledonna") score = 10;
-  giSearch(waifu).pipe(fs.createWriteStream('waifu.jpg'));
-  // const waifuEmbed = new Discord.RichEmbed()
-  //   .setColor(settings.embedColour)
-  //   .addField(waifu.toProperCase(), `${score} / 10`);
-  // message.channel.send({embed: waifuEmbed});
-  message.channel.send({file: waifu.jpg});
+  const waifuEmbed = new Discord.RichEmbed()
+    .setColor(settings.embedColour)
+    .addField(waifu.toProperCase(), `${score} / 10`);
+  message.channel.send({embed: waifuEmbed});
 };
 
 exports.cmdConfig = {
