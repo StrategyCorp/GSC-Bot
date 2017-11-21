2d596c5e29fae5a0e0635ed3f7e3efea95b38450
Glitch (aclipse) <none>
2017-11-21 13:39:29 +0000

    Checkpoint

--- commands/ratemywaifu.js
+++ commands/ratemywaifu.js
@@ -8,18 +8,25 @@ exports.run = (client, message, waifu) => {
   const settings = client.settings.get(message.guild.id);
   let score = client.randomNum(1, 10);
   if (waifu === "blake" || waifu === "blake belledonna") score = 10;
-  let url = `https://api.qwant.com/api/search/images?count=1&offset=1&q=anime&20` + waifu;
-  console.log(url);
-  request(url, function(error, response, body) {
-    const data = JSON.parse(body);
-    console.log(data);
-    // let image = data.data.result.media;
-    // console.log(image);
-    const waifuEmbed = new Discord.RichEmbed()
-    .setColor(settings.embedColour)
-    .addField(waifu.toProperCase(), `${score} / 10`);
-  message.channel.send({embed: waifuEmbed});
+  let url = `https://api.qwant.com/api/search/images?count=1&offset=1&q=anime ` + waifu;
+  request.get({
+      url: url,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        let image = data.data.result.media;
+        console.log(image);
+      }
   });
+  //   const waifuEmbed = new Discord.RichEmbed()
+  //   .setColor(settings.embedColour)
+  //   .addField(waifu.toProperCase(), `${score} / 10`);
+  // message.channel.send({embed: waifuEmbed});
 };
 
 exports.cmdConfig = {
