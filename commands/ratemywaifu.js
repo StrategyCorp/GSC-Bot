da120e2944f42929905854b722449f5244d38aa4
Glitch (aclipse) <none>
2017-11-21 13:06:29 +0000

    Checkpoint

--- commands/ratemywaifu.js
+++ commands/ratemywaifu.js
@@ -2,18 +2,19 @@ const Discord = require('discord.js');
 const giSearch = require('google-image-search');
 const fs = require('fs');
 
-exports.run = (client, message, waifu) => {
+exports.run = async (client, message, waifu) => {
   if (!waifu[0]) return message.channel.send(':negative_squared_cross_mark: You must tell me who your waifu is');
   waifu = waifu.join(' ');
   waifu = waifu.toLowerCase();
   const settings = client.settings.get(message.guild.id);
   let score = client.randomNum(1, 10);
   if (waifu === "blake" || waifu === "blake belledonna") score = 10;
-  giSearch('logo google').pipe(fs.createWriteStream('google.jpg'));
-  const waifuEmbed = new Discord.RichEmbed()
-    .setColor(settings.embedColour)
-    .addField(waifu.toProperCase(), `${score} / 10`);
-  message.channel.send({embed: waifuEmbed});
+  giSearch(waifu).pipe(fs.createWriteStream('waifu.jpg'));
+  // const waifuEmbed = new Discord.RichEmbed()
+  //   .setColor(settings.embedColour)
+  //   .addField(waifu.toProperCase(), `${score} / 10`);
+  // message.channel.send({embed: waifuEmbed});
+  message.channel.send({file: waifu.jpg});
 };
 
 exports.cmdConfig = {
