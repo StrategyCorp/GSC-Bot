e376a6b3f5937218a4798c8a49c0ea38f832361a
Glitch (aclipse) <none>
2017-11-22 15:19:31 +0000

    Checkpoint

---
+++ commands/illegal.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+exports.run = async (client, message, word) => {
+  if (!word[0]) return message.channel.send(':negative_squared_cross_mark: What would you like to be illegal?');
+  let url = `https://is-now-illegal.firebaseio.com/gifs/${word.join('').toUpperCase()}.json`
+  request.get({
+      url: url,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+      } else {
+        
+      }
+  });
+};
+
+exports.cmdConfig = {
+  name: "illegal",
+  aliases: ['isn', 'isnowillegal'],
+  description: "Makes a given thing illegal",
+  usage: "illegal <word>",
+  type: "fun"
+};
