11bc1bef6a8dbc1f3044bb445353d989ae6a2455
Glitch (aclipse) <none>
2017-11-22 08:55:32 +0000

    Checkpoint

--- commands/mock.js
+++ commands/mock.js
@@ -1,6 +1,9 @@
 exports.run = (client, message, args) => {
   if (!args[0]) {
-    message.channel.
+    message.channel.fetchMessages({limit: 2, around: message.id}).then(msgs => {
+      let messageToMock = msgs.last().content;
+      return message.channel.send(messageToMock.toMemeCase());
+    })
   } else if (args[0].length === 18 && /^\d+$/.test(args[0])) {
     message.channel.fetchMessage(args[0]).then(msg => {
       let messageToMock = msg.content;
