4ad1c5951b8e88c790b65bfb5d5ab14e85456612
Glitch (aclipse) <none>
2017-11-22 11:39:31 +0000

    Checkpoint

--- commands/image.js
+++ commands/image.js
@@ -1,8 +1,22 @@
+const canvas = require('canvas');
+const fs = require('fs');
+
 exports.run = (client, message, args) => {
   if (!args[0]) {
     return message.channel.send('HELP WIP');
+  } else {
+    let imageObj = {
+    "bonobo": "1",
+    "test": "2",
+    "anothertest": "3"
+    }
+    let imageArray = Object.keys(imageObj);
+    if (imageArray.indexOf(args.slice(1).join(' ')) > -1) {
+      
+    } else {
+
+    }
   }
-  
 };
 
 exports.cmdConfig = {
