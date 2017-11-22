029533ebaf509f6eff00c7b23baf2057e2b283bc
Glitch (aclipse) <none>
2017-11-22 06:32:32 +0000

    Checkpoint

--- util/functions.js
+++ util/functions.js
@@ -23,23 +23,23 @@ module.exports = (client) => {
     score.level = Math.floor(0.1 * Math.sqrt(score.points));
     client.points.set(message.author.id, score);
     
-    // sql.get(`SELECT * FROM ${message.guild.id} WHERE userId ="${message.author.id}"`).then(row => {
-    //   if (!row) {
-    //     sql.run(`INSERT INTO ${message.guild.id} (userId, points, level) VALUES (?, ?, ?)`, [message.author.id, 1, 0]);
-    //   } else {
-    //     let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
-    //     if (curLevel > row.level) {
-    //       row.level = curLevel;
-    //       sql.run(`UPDATE ${message.guild.id} SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
-    //     }
-    //     sql.run(`UPDATE ${message.guild.id} SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
-    //   }
-    // }).catch(() => {
-    //   console.error;
-    //   sql.run(`CREATE TABLE IF NOT EXISTS ${message.guild.id} (userId TEXT, points INTEGER, level INTEGER)`).then(() => {
-    //     sql.run(`INSERT INTO ${message.guild.id} (userId, points, level) VALUES (?, ?, ?)`, [message.author.id, 1, 0]);
-    //   });
-    // });
+    sql.get(`SELECT * FROM ${message.guild.id} WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) {
+        sql.run(`INSERT INTO ${message.guild.id} (userId, points, level) VALUES (?, ?, ?)`, [message.author.id, 1, 0]);
+      } else {
+        let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
+        if (curLevel > row.level) {
+          row.level = curLevel;
+          sql.run(`UPDATE ${message.guild.id} SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
+        }
+        sql.run(`UPDATE ${message.guild.id} SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
+      }
+    }).catch(() => {
+      console.error;
+      sql.run(`CREATE TABLE IF NOT EXISTS ${message.guild.id} (userId TEXT, points INTEGER, level INTEGER)`).then(() => {
+        sql.run(`INSERT INTO ${message.guild.id} (userId, points, level) VALUES (?, ?, ?)`, [message.author.id, 1, 0]);
+      });
+    });
   };
 
   
