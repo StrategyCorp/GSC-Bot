82db7972d0eb8aa80d375569ff875600b884b3f3
Glitch (aclipse) <none>
2017-11-22 07:38:32 +0000

    Checkpoint

--- commands/mock.js
+++ commands/mock.js
@@ -1,7 +1,13 @@
 exports.run = (client, message, args) => {
   if (!args[0]) return message.channel.send(':negative_squared_cross_mark: you must give me text to mock');
-  if (args[0].length === 18) console.log("2");
-  return message.channel.send(args.join(' ').toMemeCase());
+  if (args[0].length === 18 && /^\d+$/.test(args[0])) {
+    message.channel.fetchMessages({around: args[0], limit: 1}).then(messages => {
+      var fetchedMessage = messages.first();
+      var messageToMock = fetchedMessage.content;
+      return message.channel.send(messageToMock);
+    });
+  }
+  // return message.channel.send(args.join(' ').toMemeCase());
 };
 
 exports.cmdConfig = {
