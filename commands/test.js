65e849d15c742e3aa234082d7061db7135bdda14
Glitch (aclipse) <none>
2017-11-25 13:25:50 +0000

    Checkpoint

--- commands/test.js
+++ commands/test.js
@@ -5,30 +5,37 @@ const md5 = require('md5');
 const moment = require('moment');
 
 exports.run = (client, message, args) => {
-  let playerName = 'Gazder';
+  const domain = "http://api.smitegame.com/smiteapi.svc/";
   const devID = process.env.SMITEDEVID;
   const authKey = process.env.SMITEAUTHID;
-  let method = 'getplayerstatus';
   let timestamp = moment().format('YYYYMMDDHHmmss');
   let signature = `${devID}createsession${authKey}${timestamp}`;
-  console.log(signature)
   signature = md5(signature);
-  console.log(signature);
-  let createsession = `http://api.smitegame.com/smiteapi.svc/createsessionJson/${devID}/${signature}/${timestamp}`;
-  let url = `http://api.smitegame.com/smiteapi.svc/${method}json/${devID}/${signature}/${timestamp}/${playerName}`;
-  request.get({
-    url: createsession,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err, res, data) => {
-    if (err) {
-      return message.channel.send(':negative_squared_cross_mark: Error:', err);
-    } else if (res.statusCode !== 200) {
-      return message.channel.send(':negative_squared_cross_mark: Status:', res.statusCode);
-    } else {
-      var sessionID = data.session_id;
-    }
-  });
+  let createSessionUrl = domain + `createsessionJson/${devID}/${signature}/${timestamp}`;
+  const createSession = async (url) => {
+    request.get({
+      url: url,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        return message.channel.send(':negative_squared_cross_mark: Error:', err);
+      } else if (res.statusCode !== 200) {
+        return message.channel.send(':negative_squared_cross_mark: Status:', res.statusCode);
+      } else {
+        var session = data.session_id;
+      }
+    });
+  }
+  
+  let testSessionUrl = domain + `testsessionJson/${devID}/${signature}/${session}/${timestamp}`;
+  const testSession = async (url) => {
+    
+  }
+  
+  let method = 'getplayerstatus';
+  let playerName = 'Gazder';
+  let url = domain + `${method}json/${devID}/${signature}/${timestamp}/${playerName}`;
 }
 
 exports.cmdConfig = {
