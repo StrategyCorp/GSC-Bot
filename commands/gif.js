4e6d55d7f39566e0b21c4836eac3a9ad56c11aa2
Glitch (aclipse) <none>
2017-11-25 11:46:51 +0000

    Checkpoint

--- commands/gif.js
+++ commands/gif.js
@@ -4,8 +4,14 @@ exports.run = (client, message, [search, ...args]) => {
   if (!search) {
     return message.channel.send('HELP WIP');
   } else if (search === "search") {
-    let q = args.join(' ');
     let lastItem = args[args.length - 1];
+    if (/^\d+$/.test(lastItem)) {
+      var q = args.splice(-1, 1).join(' ');
+    } else {
+      var q = args.join(' ');
+      lastItem = 1;
+    }
+    console.log(q);
     console.log(lastItem);
   } else if (search === "trending") {
     
