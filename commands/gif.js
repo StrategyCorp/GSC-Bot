fda04f4425b67bcb85f466f9b33461e2d2a314ad
Glitch (aclipse) <none>
2017-11-25 19:33:52 +0000

    Checkpoint

--- commands/gif.js
+++ commands/gif.js
@@ -5,12 +5,20 @@ exports.run = (client, message, [search, ...args]) => {
   const apiKey = process.env.GIPHY;
   if (!search) {
     var cmdArray = [
-      ["search", ""],
-      ["'trending'"],
-      ['random']
-    ]
-    return message.channel.send('HELP WIP');
+      ["search", "<term> [number]", "Gives a gif from a given term"],
+      ["trending", "[number]", "Gives the trending gifs on Giphy"],
+      ["random", "<term>", "Gives a random gif from a given term"]
+    ];
+    const settings = client.settings.get(message.guild.id);
+    const helpEmbed = new Discord.RichEmbed()
+      .setColor(settings.embedColour)
+      .setTitle('**Gif Help**');
+    for (let [cmdName, cmdUsage, cmdDesc] of cmdArray) {
+       helpEmbed.addField(cmdName, `${settings.prefix}dashboard ${cmdName} ${cmdUsage}\n${cmdDesc}`);
+    }
+    return message.channel.send({embed: helpEmbed});
   } else if (search === "search") {
+    if (!args[0]) return message.channel.send(':negative_squared_cross_mark: You must give me a term to search');
     var offset = args[args.length - 1];
     if (/^\d+$/.test(offset)) {
       args.pop();
@@ -32,6 +40,7 @@ exports.run = (client, message, [search, ...args]) => {
     offset = offset - 1;
     var url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25&rating=G`;
   } else if (search === "random") {
+    if (!args[0]) return message.channel.send(':negative_squared_cross_mark: You must give me a term to search');
     var q = args.join(' ');
     var url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${q}&rating=G`;
   }
