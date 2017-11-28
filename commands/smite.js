ac6056fd664e7602dfeb0cd4f47447f7c9de9a3b
Glitch (aclipse) <none>
2017-11-28 21:07:02 +0000

    Checkpoint

--- commands/smite.js
+++ commands/smite.js
@@ -237,7 +237,11 @@ exports.run = async (client, message, [search, ...args]) => {
         } else if (search === "items") {
           const findItemByName = (searchItem) => {
             return searchItem["DeviceName"].toLowerCase() === args.join(' ').toLowerCase();
-          }
+          };
+          let searchID;
+          const findItemByID = (searchID) => {
+            return searchID.ItemId === searchID;
+          };
           var i = data.find(findItemByName);
           if (!i) return message.channel.send(`:negative_squared_cross_mark: \`${args.join(' ')}\` is not an item`);
           let stats = [];
@@ -249,9 +253,16 @@ exports.run = async (client, message, [search, ...args]) => {
           ];
           if (i.StartingItem) {
             main.unshift(`**Item Tier:** Starter`);
-            main.unshift(`**Price:** ${i.Price}``)
+            main.unshift(`**Price:** ${i.Price}`);
           } else {
             main.unshift(`**Item Tier:** ${i.ItemTier}`);
+            if (i.ItemTier === 1) {
+              main.unshift(`**Price:** ${i.Price}`);
+            } else if (i.ItemTier === 2) {
+              searchID = i.ChildItemId;
+              var child = data.find(findItemByID);
+              main.unshift(`**Price:** ${child.Price} + ${i.Price}`);
+            }
           }
           if (i.ItemDescription.SecondaryDescription !== "" || i.ItemDescription.SecondaryDescription !== null) {
             main.unshift(`**Effect:** ${i.ItemDescription.SecondaryDescription}`);
