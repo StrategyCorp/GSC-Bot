f577c4580d4d51fde07d70260cc6c196f5351061
Glitch (aclipse) <none>
2017-12-02 15:26:27 +0000

    🎇🚞 Checkpoint commands/memes.js:529286 data/memes.json:529286

--- commands/memes.js
+++ commands/memes.js
@@ -1,7 +1,25 @@
 const request = require('request');
+const meme = require('../data/memes.json');
 
 exports.run = (client, message, [search, ...args]) => {
-  request.get()
+  if (search === "list") {
+    var list = Object.keys(meme.inbuild);
+    return message.channel.send(list.join(', '));
+  }
+  let url = 'https://memegen.link/buzz/memes/memes_everywhere';
+  request.get({
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      return message.channel.send(':negative_squared_cross_mark: Error: ' + err);
+    } else if (res.statusCode !== 200) {
+      return message.channel.send(':negative_squared_cross_mark: Status: ' + res.statusCode);
+    } else {
+      return message.channel.send({'files': [data.direct.masked]});
+    }
+  });
 };
 
 exports.cmdConfig = {
