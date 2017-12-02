51f73773e622b47bf1926278d3bb71cfa8370a08
Glitch (aclipse) <none>
2017-12-02 17:05:27 +0000

    🌹👹 Checkpoint commands/memes.js:529286 commands/illegal.js:529286 package.json:529286 commands/help.js:529286

--- commands/memes.js
+++ commands/memes.js
@@ -9,23 +9,22 @@ exports.run = (client, message, args) => {
     return message.channel.send(`**Page Number ${pageNumber} of ${list.length}**\n${list[pageNumber - 1].join(', ')}`);
   }
   list = list.join('-=_').toLowerCase().split('-=_');
-  args = args.join(' ').split(' | ');
-  console.log(args);
-  if (client.isInArray(list, args[0].toLowerCase()) === false) return message.channel.send(':negative_squared_cross_mark: Error 404 meme not found');
+  args = args.join(' ').split('|');
+  if (client.isInArray(list, args[0].toLowerCase().trim()) === false) return message.channel.send(':negative_squared_cross_mark: Error 404 meme not found');
   args[1] = !args[1] ? "_" : args[1];
   args[2] = !args[2] ? "_" : args[2];
   let url = `https://memegen.link/${args[0]}/${args[1]}/${args[2]}`;
   request.get({
-    url: url
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
   }, (err, res, data) => {
     if (err) {
       return message.channel.send(':negative_squared_cross_mark: Error: ' + err);
     } else if (res.statusCode !== 200) {
       return message.channel.send(':negative_squared_cross_mark: Status: ' + res.statusCode);
     } else {
-      console.log(data);
-      message.channel.send(data.f)
-      // return message.channel.send({'files': [data.direct.visible]});
+      return message.channel.send({'files': [data.direct.visible]});
     }
   });
 };
@@ -33,7 +32,7 @@ exports.run = (client, message, args) => {
 exports.cmdConfig = {
   name: "memes",
   aliases: ['meme'],
-  description: "Makes a memes.",
-  usage: "memes",
+  description: "Makes a memes. To view all available memes use !memes list",
+  usage: "memes list || [<meme> | [top line] | [bottom line]]",
   type: "fun"
 };
