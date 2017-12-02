96633063c1bc14f6f180e5d1327bf4970d13b547
Glitch (aclipse) <none>
2017-12-02 16:54:27 +0000

    🎁🌈 Checkpoint commands/memes.js:529286

--- commands/memes.js
+++ commands/memes.js
@@ -8,8 +8,9 @@ exports.run = (client, message, args) => {
     let pageNumber = args[1] ? (/^\d+$/.test(args[1]) ? (args[1] > list.length ? list.length : args[1]) : 1) : 1;
     return message.channel.send(`**Page Number ${pageNumber} of ${list.length}**\n${list[pageNumber - 1].join(', ')}`);
   }
-  list = list.join('|+|').toLowerCase().split('|+|');
+  list = list.join('-=_').toLowerCase().split('-=_');
   args = args.join(' ').split(' | ');
+  console.log(args);
   if (client.isInArray(list, args[0].toLowerCase()) === false) return message.channel.send(':negative_squared_cross_mark: Error 404 meme not found');
   args[1] = !args[1] ? "_" : args[1];
   args[2] = !args[2] ? "_" : args[2];
@@ -22,7 +23,9 @@ exports.run = (client, message, args) => {
     } else if (res.statusCode !== 200) {
       return message.channel.send(':negative_squared_cross_mark: Status: ' + res.statusCode);
     } else {
-      return message.channel.send({'files': [data.direct.masked]});
+      console.log(data);
+      message.channel.send(data.f)
+      // return message.channel.send({'files': [data.direct.visible]});
     }
   });
 };
