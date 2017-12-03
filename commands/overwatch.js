13d305e8ea4f6746095124c981106433d7bbe3ff
Glitch (aclipse) <none>
2017-12-03 17:48:27 +0000

    🛫👲 Checkpoint commands/overwatch.js:529286

--- commands/overwatch.js
+++ commands/overwatch.js
@@ -101,42 +101,27 @@ exports.run = async (client, message, [search, ...args]) => {
         const statGame = (p) => {
           let total = [];
           let most = [];
-          const stat = (title, stat) => {
-            total.push(`- ${title}: ${p[stat]}`)
+          let stat = [
+            ['Kills', 'eliminations'],
+            ['Last Hits', 'final_blows'],
+            ['Melee', 'melee_final_blow'],
+            ['Solo', 'solo_kills'],
+            ['Objective', 'objective_kills'],
+            ['Multikills', 'multikills'],
+            ['Offensive Assists', 'offensive_assists'],
+            ['Defensive Assists', 'defensive_assists'],
+            ['Recon Assits', 'recon_assists'],
+            ['Healing', 'healing_done'],
+            ['Damage Done', 'all_damage_done'],
+            ['Barrier Damage Done', 'barrier_damage_done']
+          ];
+          for (let [title, value] of stat) {
+            total.push(`- ${title}: ${p[value]}`);
+            most.push(`- ${title}: ${p[value + "_most_in_game"]}`);
           }
-          // let total = [
-          //   `- Kills: ${p.eliminations}`,
-          //   `- Last Hits: ${p.final_blows}`,
-          //   `- Melee: ${p.melee_final_blows}`,  
-          //   `- Solo: ${p.solo_kills}`, 
-          //   `- Objective: ${p.objective_kills}`,  
-          //   `- Multikills: ${p.multikills}`,
-          //   `- Offensive Assists: ${p.offensive_assists}`,
-          //   `- Defensive Assists: ${p.defensive_assists}`,
-          //   `- Recon Assits: ${p.recon_assists}`,
-          //   `- Healing: ${p.healing_done}`,
-          //   `- Damage Done: ${p.all_damage_done}`,
-          //   `- Barrier Damage Done: ${p.barrier_damage_done}`,
-          //   `- Time on Fire: ${client.roundToTwo(p.time_spent_on_fire)}`
-          // ];
-          // let most = [
-          //   `- Kills: ${p.eliminations_most_in_game}`,
-          //   `- Last Hits: ${p.final_blows_most_in_game}`,
-          //   `- Melee: ${p.melee_final_blow_most_in_game}`,
-          //   `- Solo: ${p.solo_kills_most_in_game}`,
-          //   `- Objective: ${p.objective_kills_most_in_game}`, 
-          //   `- Multikills: ${p.multikills_most_in_game}`,
-          //   `- Offensive Assists: ${p.offensive_assists_most_in_game}`,
-          //   `- Defensive Assists: ${p.defensive_assists_most_in_game}`,
-          //   `- Recon Assits: ${p.recon_assists_most_in_game}`,
-          //   `- Healing: ${p.healing_done}`,
-          //   `- Damage Done: ${p.all_damage_done_most_in_game}`,
-          //   `- Barrier Damage Done: ${p.barrier_damage_done_most_in_game}`,
-          //   `- Time on Fire: ${client.roundToTwo(p.time_spent_on_fire_most_in_game)}`
-          // ];
           let stats = [
-            `**Total:** ${total.join('\n')}`,
-            `\n**Most in One Match:** ${most.join('\n')}`,
+            `**Total:**\n${total.join('\n')}`,
+            `\n**Most in One Match:**\n${most.join('\n')}`,
             `\n**Time Played:** ${p.time_played} Hours`,
             `**Kills per Death:** ${p.kpd}`,
             `**Deaths:** ${p.deaths}`,
