85916a04d86acc450604fce56eaaccf0d6f08572
Glitch (aclipse) <none>
2017-12-03 12:29:27 +0000

    📫🚲 Checkpoint commands/smite.js:529286 commands/illegal.js:529286 commands/overwatch.js:529286

---
+++ commands/overwatch.js
@@ -0,0 +1,13 @@
+const request = require('request');
+
+exports.run = async (client, message, [search, ...args]) => {
+  
+};
+
+exports.cmdConfig = {
+  name: "overwatch",
+  aliases: ['ow'],
+  description: "Gives Overwatch stats",
+  usage: "overwatch",
+  type: "info"
+};
