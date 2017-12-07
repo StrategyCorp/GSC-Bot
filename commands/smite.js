a6c7d49af7b2e0783322ebc1a05ff9c28faae1f3
Glitch (aclipse) <none>
2017-12-07 04:28:09 +0000

    👢💪 Checkpoint commands/smite.js:529286

--- commands/smite.js
+++ commands/smite.js
@@ -199,23 +199,25 @@ exports.run = async (client, message, [search, ...args]) => {
       return message.channel.send({embed: helpEmbed});
     } else if (search === "joke") {
       var jokeArrayArray = [
-        ["Why does everyone think Bacchus is so annoying?", "Because he's always whining"],
-        ["What is Sol's favourite movie?", "Twilight: Breaking Down"],
-        ["Why does everyone think that Xing Tian uses drugs?", "Because he's always Xing things"],
-        ["What type of camera does Vulcan use?", "A cannon"],
-        ["Why does Medusa make the best weed dealer?", "She loves to help people get stoned"],
-        ["Did you hear He Bo used to be a celebrity?", "Now he's all washed up"],
-        ["I told a joke about Awilix to my friend,", "but it went over his head"],
-        ["Why isn't Sobek ever in a serious relationship?", "Because all he's ever looking for is a fling"],
-        ["What's Bellona's favourite restaurant?", "Taco Bellona"],
-        ["Rexsi's winrate", ". . ."]
+        ["Why does everyone think Bacchus is so annoying?", "Because he's always whining", "/u/MaggehG"],
+        ["What is Sol's favourite movie?", "Twilight: Breaking Down", "/u/MaggehG"],
+        ["Why does everyone think that Xing Tian uses drugs?", "Because he's always Xing things", "/u/MaggehG"],
+        ["What type of camera does Vulcan use?", "A cannon", "/u/barblebapkins"],
+        ["Why does Medusa make the best weed dealer?", "She loves to help people get stoned", "/u/barblebapkins"],
+        ["Did you hear He Bo used to be a celebrity?", "Now he's all washed up", "/u/barblebapkins"],
+        ["I told a joke about Awilix to my friend,", "but it went over his head", "/u/barblebapkins"],
+        ["Why isn't Sobek ever in a serious relationship?", "Because all he's ever looking for is a fling", "/u/xdapenguinx"],
+        ["What's Bellona's favourite restaurant?", "Taco Bellona", "/u/MaggehG"],
+        ["Rexsi's winrate", 166641492113358848]
       ];
       let jokeArray = Object.keys(jokeArrayArray);
-      let jokeNumber = /^\d+$/.test(args[0]) ? (args[0]) : client.randomNum(1, jokeArrayArray.length);
+      let jokeNumber = /^\d+$/.test(args[0]) ? (jokeArrayArray.length < args[0]) ? client.randomNum(1, jokeArrayArray.length) : args[0] : client.randomNum(1, jokeArrayArray.length);
+      let credit = jokeArrayArray[jokeNumber - 1]
       const jokeEmbed = new Discord.RichEmbed()
         .setColor(settings.embedColour)
-        // .addField(`:regional_indicator_q: ${joke}`, `:regional_indicator_a: ${jokeArrayArray[joke]}`)
         .setFooter(`#${jokeNumber}`);
+      if (jokeArrayArray[jokeNumber - 1].length === 1) jokeEmbed.setTitle(jokeArrayArray[jokeNumber - 1][0]);
+      if (jokeArrayArray[jokeNumber - 1].length === 2) jokeEmbed.addField(`:regional_indicator_q: ${jokeArrayArray[jokeNumber - 1][0]}`, `:regional_indicator_a: ${jokeArrayArray[jokeNumber - 1][1]}`);
       return message.channel.send({embed: jokeEmbed});
     }
   }
