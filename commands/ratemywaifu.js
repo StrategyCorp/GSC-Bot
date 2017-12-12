32f773b3123002117bd19e1166e7146e5c06f4cf
Glitch (aclipse) <none>
2017-12-12 10:04:37 +0000

    🙅☎️ Checkpoint

--- commands/ratemywaifu.js
+++ commands/ratemywaifu.js
@@ -11,23 +11,22 @@ exports.run = (client, message, waifu) => {
   if (waifu === "blake" || waifu === "blake belladonna") score = 10;
   var url = `https://api.qwant.com/api/search/images?count=1&offset=1&q=anime ` + waifu;
   request.get({
-      url: url,
-      json: true,
-      headers: {'User-Agent': 'request'}
-    }, (err, res, data) => {
-      if (err) {
-        return message.channel.send(':negative_squared_cross_mark: Error: ' + err);
-      } else if (res.statusCode !== 200) {
-        return message.channel.send(':negative_squared_cross_mark: Status: ' + res.statusCode);
-      } else {
-        var image = data.data.result["items"].length > 0 ? data.data.result.items[0].media : null;
-        const waifuEmbed = new Discord.RichEmbed()
-      .setColor(settings.embedColour)
-      .addField(waifu.toProperCase(), `${score} / 10`);
-    if (image !== null) waifuEmbed.setImage(image);
-    return message.channel.send({embed: waifuEmbed});
-      }
-    
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      return message.channel.send(':negative_squared_cross_mark: Error: ' + err);
+    } else if (res.statusCode !== 200) {
+      return message.channel.send(':negative_squared_cross_mark: Status: ' + res.statusCode);
+    } else {
+      var image = data.data.result["items"].length > 0 ? data.data.result.items[0].media : null;
+      const waifuEmbed = new Discord.RichEmbed()
+        .setColor(settings.embedColour)
+        .addField(waifu.toProperCase(), `${score} / 10`);
+      if (image !== null) waifuEmbed.setImage(image);
+      return message.channel.send({embed: waifuEmbed});
+    }
   });
 };
 
