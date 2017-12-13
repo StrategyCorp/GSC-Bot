b3c8d103bbd8b4e3fece661d5fe46e3687603ad1
Glitch (aclipse) <none>
2017-12-13 08:34:29 +0000

    🛥🚏 Checkpoint data/waifu.json:529286 commands/ratemywaifu.js:529286

--- commands/ratemywaifu.js
+++ commands/ratemywaifu.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const db = require('../data/waifu.json');
 const request = require('request');
 const md5 = require('md5');
 
@@ -9,25 +10,33 @@ exports.run = (client, message, waifu) => {
   var score = md5(waifu.toLowerCase()).replace(/\D/g,'');
   score = 10 - parseInt(score.charAt(0));
   if (waifu === "blake" || waifu === "blake belladonna") score = 10;
-  var url = `https://api.qwant.com/api/search/images?count=1&offset=1&q=anime ` + waifu;
-  request.get({
-    url: url,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err, res, data) => {
-    if (err) {
-      return message.channel.send(':negative_squared_cross_mark: Error: ' + err);
-    } else if (res.statusCode !== 200) {
-      return message.channel.send(':negative_squared_cross_mark: Status: ' + res.statusCode);
-    } else {
-      var image = data.data.result["items"].length > 0 ? data.data.result.items[0].media : null;
-      const waifuEmbed = new Discord.RichEmbed()
-        .setColor(settings.embedColour)
-        .addField(waifu.toProperCase(), `${score} / 10`);
-      if (image !== null) waifuEmbed.setImage(image);
-      return message.channel.send({embed: waifuEmbed});
-    }
-  });
+  var waifus = Object.keys(db);
+  if (client.isInArray(waifus, waifu)) {
+    embed(db[waifu]);
+  } else {
+    var url = `https://api.qwant.com/api/search/images?count=1&offset=1&q=anime ` + waifu;
+    request.get({
+      url: url,
+      json: true,
+      headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+      if (err) {
+        return message.channel.send(':negative_squared_cross_mark: Error: ' + err);
+      } else if (res.statusCode !== 200) {
+        return message.channel.send(':negative_squared_cross_mark: Status: ' + res.statusCode);
+      } else {
+        var image = data.data.result["items"].length > 0 ? data.data.result.items[0].media : null;
+        embed(image);
+      }
+    });
+  }
+  function embed(image) {
+    const waifuEmbed = new Discord.RichEmbed()
+      .setColor(settings.embedColour)
+      .addField(waifu.toProperCase(), `${score} / 10`);
+    if (image !== null) waifuEmbed.setImage(image);
+    return message.channel.send({embed: waifuEmbed});
+  }
 };
 
 exports.cmdConfig = {
