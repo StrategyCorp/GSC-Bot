b5db3cf0186febaf57ae9754c4b2b77fbb8b74c4
Glitch (aclipse) <none>
2017-12-17 22:19:59 +0000

    🐟🌂 Checkpoint commands/smite.js:529286

--- commands/smite.js
+++ commands/smite.js
@@ -405,15 +405,26 @@ exports.run = async (client, message, [search, ...args]) => {
           var masteryEmbed = new Discord.RichEmbed()
             .setColor(settings.embedColour)
             .addField(`${user.toProperCase()}'${s} stats for ${g.god}`, main(g));
-        } else {      
+        } else {
+          let pages = client.chunkArray(m, 5);
+          let pageNumber = args[1] ? (/^\d+$/.test(args[1]) ? (args[1] > pages.length ? pages.length : (args[1] === "0" ? 1 : args[1])) : 1) : 1;
           var masteryEmbed = new Discord.RichEmbed()
             .setColor(settings.embedColour)
             .setTitle(`${user}'${s} Masteries`);
-          let number = /^\d+$/.test(args) ? (args > 19) ? 20 : args : 5;
-          for (var i = 0; i < number; i++) {
-            if (m.length > 0) {
-              let hm = m.shift();
-              masteryEmbed.addField(hm.god, main(hm));
+          // let number = /^\d+$/.test(args) ? (args > 19) ? 20 : args : 5;
+          // for (var i = 0; i < number; i++) {
+          //   if (m.length > 0) {
+          //     let hm = m.shift();
+          //     masteryEmbed.addField(hm.god, main(hm));
+          //   }
+          // }
+          var historyEmbed = new Discord.RichEmbed()
+            .setColor(settings.embedColour)
+            .addField(`${args[0]}'${s} History`, `Page ${pageNumber} of ${pages.length}`);
+          for (var i = 0; i < 5; i++) {
+            if (pages[pageNumber - 1].length > 0) {
+              let m = pages[pageNumber - 1].shift();
+              masteryEmbed.addField(m.god, main(m));
             }
           }
         }
