eb0020fe702131b9e5002e6c7aee9ac11e70dc52
Glitch (aclipse) <none>
2018-01-20 12:32:03 +0000

    🏎🐆 Checkpoint commands/fight.js:529286/438

--- commands/fight.js
+++ commands/fight.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
   if (!client.hasOwnProperty('fight')) client.fight = {};
   if (!client.fight.hasOwnProperty(message.guild.id)) {client.fight[message.guild.id] = {active: false};};
   if (client.fight[message.guild.id].active === true) return message.channel.send(`:negative_squared_cross_mark: A fight is already taking place between **${client.users.get(client.fight[message.guild.id].first).username}** and **${client.users.get(client.fight[message.guild.id].second).username}**. We don't want it to be a bloodbath, do we?`);
-  let rounds = args[0] ? (/^\d+$/.test(args[0]) ? (1 < args[0] ? (20 > args[0] ? args[0] : 3) : 3) : 3) : 3;
+  let rounds = args[0] ? (/^\d+$/.test(args[0]) ? (1 < args[0] ? (20 > args[0] ? (args[0] % 2 === 0 ? parseInt(args[0]) + 1 : args[0]) : 3) : 3) : 3) : 3;
   let user = message.mentions.users.first();
   if (user.id === message.author.id) return message.channel.send(':negative_squared_cross_mark: You may not fight yourself');
   if (message.mentions.users.size < 1) return message.channel.send(':negative_squared_cross_mark: You must mention someone to fight them');
@@ -24,31 +24,38 @@ exports.run = (client, message, args) => {
         round: 1,
         score: [0, 0]
       };
-      const collector = message.channel.createMessageCollector(message => message);
-      collector.on('collect', msg => {
-        if (msg.content.startsWith('roll') && (msg.author.id === client.fight[message.guild.id].first || msg.author.id === client.fight[message.guild.id].second)) {
-          let person = msg.author.id === client.fight[message.guild.id].first ? 'first' : 'second';
-          if (client.fight[message.guild.id][person + 'Score'] === null) {
-            let score = client.randomNum(1, 100);
-            client.fight[message.guild.id][person + 'Score'] = score;
-            message.channel.send(`:crossed_swords: **${msg.author.username}** has rolled \`${score}\``);
-            console.log(client.fight[message.guild.id]);
-            if (client.fight[message.guild.id].firstScore !== null && client.fight[message.guild.id].secondScore !== null) {
-              if (client.fight[message.guild.id].firstScore === client.fight[message.guild.id].secondScore) {
-                message.channel.send(':crossed_swords: The round was a draw and will be redone');                                                                                        
-              } else if (client.fight[message.guild.id].firstScore > client.fight[message.guild.id].secondScore) {
-                client.fight[message.guild.id].score[0] = client.fight[message.guild.id].score[0] + 1;
-                message.channel.send(`:crossed_swords: **${client.users.get(client.fight[message.guild.id].first).username}** has won the round. The score is now \`${client.fight[message.guild.id].score[0]} - ${client.fight[message.guild.id].score[1]}\``);
-              } else if (client.fight[message.guild.id].firstScore < client.fight[message.guild.id].secondScore) {
-                client.fight[message.guild.id].score[1] = client.fight[message.guild.id].score[1] + 1;
-                message.channel.send(`:crossed_swords: **${client.users.get(client.fight[message.guild.id].second).username}** has won the round. The score is now \`${client.fight[message.guild.id].score[0]} - ${client.fight[message.guild.id].score[1]}\``);
+      makeRound();
+      function makeRound () {
+        const collector = message.channel.createMessageCollector(message => message);
+        collector.on('collect', msg => {
+          if (msg.content.startsWith('roll') && (msg.author.id === client.fight[message.guild.id].first || msg.author.id === client.fight[message.guild.id].second)) {
+            let person = msg.author.id === client.fight[message.guild.id].first ? 'first' : 'second';
+            if (client.fight[message.guild.id][person + 'Score'] === null) {
+              let score = client.randomNum(1, 100);
+              client.fight[message.guild.id][person + 'Score'] = score;
+              message.channel.send(`:crossed_swords: **${msg.author.username}** has rolled \`${score}\``);
+              console.log(client.fight[message.guild.id]);
+              if (client.fight[message.guild.id].firstScore !== null && client.fight[message.guild.id].secondScore !== null) {
+                const endMessage = (name, score) => {
+                  
+                }
+                if (client.fight[message.guild.id].firstScore === client.fight[message.guild.id].secondScore) {
+                  message.channel.send(':crossed_swords: The round was a draw and will be redone');                                                                                        
+                } else if (client.fight[message.guild.id].firstScore > client.fight[message.guild.id].secondScore) {
+                  client.fight[message.guild.id].score[0] = client.fight[message.guild.id].score[0] + 1;
+                  message.channel.send(`:crossed_swords: **${client.users.get(client.fight[message.guild.id].first).username}** has won the round. The score is now \`${client.fight[message.guild.id].score[0]} - ${client.fight[message.guild.id].score[1]}\``);
+                } else if (client.fight[message.guild.id].firstScore < client.fight[message.guild.id].secondScore) {
+                  client.fight[message.guild.id].score[1] = client.fight[message.guild.id].score[1] + 1;
+                  message.channel.send(`:crossed_swords: **${client.users.get(client.fight[message.guild.id].second).username}** has won the round. The score is now \`${client.fight[message.guild.id].score[0]} - ${client.fight[message.guild.id].score[1]}\``);
+                }
+                collector.stop();
               }
+            } else {
+              message.channel.send(`:crossed_swords: You have already rolled for this round. You scored \`${client.fight[message.guild.id][person + 'Score']}\``);
             }
-          } else {
-            message.channel.send(`:crossed_swords: You have already rolled for this round. You scored \`${client.fight[message.guild.id][person + 'Score']}\``);
           }
-        }
-      });
+        });
+      }
     }
   });
 };
