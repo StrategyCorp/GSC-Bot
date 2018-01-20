0db3f1e3886088e359b3461549d87268264b284f
Glitch (aclipse) <none>
2018-01-20 10:08:50 +0000

    🦂🐏 Checkpoint commands/fight.js:529286/267

--- commands/fight.js
+++ commands/fight.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
   if (!args[0]) return message.channel.send(':negative_squared_cross_mark: You must say have many rounds you want it');
   let user = message.mentions.users.first();
   if (message.mentions.users.size < 1) return message.channel.send(':negative_squared_cross_mark: You must mention someone to challenge them');
-  message.channel.send(``);
+  message.channel.send(`**${message.author.username}** has challenged **${user.username}** to a fight, <@${user.id}> do you accept? (yes/no)`);
 };
 
 exports.cmdConfig = {
