3ae92bca1e1a18caf46115a16dde40485631d6f5
Glitch (aclipse) <none>
2018-01-20 10:19:03 +0000

    🍖🚔 Checkpoint commands/play.js:529286/4 commands/fight.js:529286/68

--- commands/fight.js
+++ commands/fight.js
@@ -3,6 +3,15 @@ exports.run = (client, message, args) => {
   let user = message.mentions.users.first();
   if (message.mentions.users.size < 1) return message.channel.send(':negative_squared_cross_mark: You must mention someone to challenge them');
   message.channel.send(`**${message.author.username}** has challenged **${user.username}** to a fight, <@${user.id}> do you accept? (yes/no)`);
+  const collector = message.channel.createMessageCollector(message => message, {time: 15000});
+  collector.on('collect', m => {
+    if (m.content.startsWith('no')) {
+      collector.stop();
+      return message.channel.send('ended');
+    } else if (m.content.startsWith('yes')) {
+      return message.channel.send('▶ Resumed');
+    }
+  });
 };
 
 exports.cmdConfig = {
