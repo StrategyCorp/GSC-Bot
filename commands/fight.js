036818c81267da81085b92a8181246a4ae69ace6
Glitch (aclipse) <none>
2018-01-20 09:58:41 +0000

    👨‍❤️‍👨🚉 Checkpoint commands/fight.js:529286/44

--- commands/fight.js
+++ commands/fight.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
   if (!args[0]) return message.channel.send(':negative_squared_cross_mark: You must say have many rounds you want it');
   let user = message.mentions.users.first();
   if (message.mentions.users.size < 1) return message.channel.send(':negative_squared_cross_mark: You must mention someone to challenge them');
-  message.channel.send(
+  message.channel.send(``);
 };
 
 exports.cmdConfig = {
