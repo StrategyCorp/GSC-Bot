491efc25dedf63b3efe4497dbd726a635afb6df7
Glitch (aclipse) <none>
2018-01-20 10:59:48 +0000

    😿👼 Checkpoint commands/fight.js:529286/430

--- commands/fight.js
+++ commands/fight.js
@@ -1,4 +1,5 @@
 exports.run = (client, message, args) => {
+  if (client.fight[message.author.id].active === true) return message.channel.send(`:negative_squared_cross_mark: A fight is already taking place between **${client.fight[message.author.id].first}**`)
   let rounds = args[0] ? (/^\d+$/.test(args[0]) ? (1 < args[0] ? (20 > args[0] ? args[0] : 3) : 3) : 3) : 3;
   let user = message.mentions.users.first();
   if (user.id === message.author.id) return message.channel.send(':negative_squared_cross_mark: You may not fight yourself');
@@ -11,7 +12,14 @@ exports.run = (client, message, args) => {
       return message.channel.send('ended');
     } else if (m.content.startsWith('yes') && m.author.id === user.id) {
       message.channel.send('The fight will now commence! Both parties need to say `roll` to get their scores for the round');
-      
+      client.fight[message.guild.id] = {
+        active: true,
+        first: message.author.id,
+        second: user.id,
+        rounds: rounds,
+        round: 1,
+        score: [0, 0]
+      }
       const collector = message.channel.createMessageCollector(message => message);
       collector.on('collect', msg => {
         
