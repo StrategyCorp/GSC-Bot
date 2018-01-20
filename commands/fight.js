f51ce61b026be71d3dc491538ce907d92c716da0
Glitch (aclipse) <none>
2018-01-20 10:29:10 +0000

    🌴🍴 Checkpoint commands/fight.js:529286/189

--- commands/fight.js
+++ commands/fight.js
@@ -1,15 +1,16 @@
 exports.run = (client, message, args) => {
+  let rounds = 3;
   if (!args[0]) return message.channel.send(':negative_squared_cross_mark: You must say have many rounds you want it');
   let user = message.mentions.users.first();
   if (message.mentions.users.size < 1) return message.channel.send(':negative_squared_cross_mark: You must mention someone to challenge them');
   message.channel.send(`**${message.author.username}** has challenged **${user.username}** to a fight, <@${user.id}> do you accept? (yes/no)`);
   const collector = message.channel.createMessageCollector(message => message, {time: 15000});
   collector.on('collect', m => {
-    if (m.content.startsWith('no')) {
+    if (m.content.startsWith('no') && m.author.id === user.id) {
       collector.stop();
       return message.channel.send('ended');
-    } else if (m.content.startsWith('yes')) {
-      return message.channel.send('▶ Resumed');
+    } else if (m.content.startsWith('yes') && m.author.id === user.id) {
+      return message.channel.send('The fight will now commence! Both parties need to say `roll` to get their scores for the round');
     }
   });
 };
