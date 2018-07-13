dd5da8e0bcc84fe140a4fb1ee439d5450306f24d
Glitch (gsc-bot) <none>
2018-07-13 22:47:13 +0000

    🚏🏵 Checkpoint ./commands/data.js:1208261/436

--- commands/data.js
+++ commands/data.js
@@ -1,6 +1,12 @@
 var fs = require('fs');
 module.exports.run = async (bot, message, args) => {
-
+var user = message.mentions.members.first();
+message.channel.send(`${user} Data Has been saved with a timestamp and ${args}`);
+  fs.appendFile(`$"\n")
+  fs.appendFile(`${user}`, new Date(), function (err) {
+  if (err) throw err;
+  console.log(`Saved! ${user}`);
+});
 }
 
 exports.cmdConfig = {
