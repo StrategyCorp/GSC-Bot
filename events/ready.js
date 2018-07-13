d07effa69f849e7cb9cd9f4d2cbccb85a34fcd89
Glitch (trashsadfdasf) <none>
2018-07-13 21:04:39 +0000

    🐊🏯 Checkpoint ./events/ready.js:1208261/594

--- events/ready.js
+++ events/ready.js
@@ -5,25 +5,4 @@ module.exports = async client => {
   client.guilds.filter(g => !client.settings.has(g.id)).forEach(g => client.settings.set(g.id, client.config.serverSettings));
   client.guilds.filter(g => !client.spoints.has(g.id)).forEach(g => client.spoints.set(g.id, client.config.serverPoints));
   client.user.setActivity(`Serving ${client.guilds.size} GSC servers`);
-  let minutes = client.config.gameChangeTime;
-  let interval = minutes * 60 * 1000;
-  setInterval(function() {
-    let game = client.config.games;
-    game = game[Math.floor(Math.random() * game.length)];
-    client.user.setGame(game);
-  }, interval);
 };
-
-const http = require('http');
-const express = require('express');
-const app = express();
-
-app.get("/", (request, response) => {
-  //console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
