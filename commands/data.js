2fd4174092791d36d1b80d25c8b8fea28a2629fc
Glitch (gsc-bot) <none>
2018-07-13 23:29:29 +0000

    ⚽️🤒 Checkpoint ./commands/data.js:1208261/698

--- commands/data.js
+++ commands/data.js
@@ -1,18 +1,21 @@
 var fs = require('fs');
 module.exports.run = async (bot, message, args) => {
+if(message.member.roles.find("name", "Data Admin")){
+    
 var user = message.mentions.members.first();
 
   fs.appendFile(`${user}`,"\n")
   var date = new Date();
-  var dataf = date+args+message.author
+  var dataf = date+args+`\` log from \`${message.author}`
   var datafs = dataf.replace(/,/g," ");
   message.channel.send(`${user} Data Has been saved with this data \n ${datafs}`);
   fs.appendFile(`${user}`, datafs , function (err) {
   if (err) throw err;
   console.log(`Saved! ${user}`);
 });
-}
-
+}else{
+  message.channel.send(`${message.author} You do not have the required permissions to do this!`);
+}}
 exports.cmdConfig = {
   name: "data",
   aliases: ['d','data', 'ort'],
