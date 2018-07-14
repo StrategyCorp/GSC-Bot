bbcc30336e35f2e7e9a5cadf26a5a8752bf309d0
Glitch (gsc-bot) <none>
2018-07-14 05:10:38 +0000

    🌌🐂 Checkpoint ./app.js:1208261/1281 ./events/guildMemberAdd.js:1208261/1699

--- app.js
+++ app.js
@@ -32,10 +32,10 @@ const init = async () => {
     client.on(eventName, event.bind(null, client));
     delete require.cache[require.resolve(`./events/${file}`)];
   });
-  bot.on('guildMemberAdd', member => {
-  console.log('User
-  }
+}
+
   client.login(process.env.TOKEN);
-};
+  
+         
 
 init();
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,17 +1,8 @@
-module.exports = (client, member) => {
-  const settings = client.settings.get(member.guild.id);
-  if (settings.welcomeEnabled === "true") {
-    let welcomeMessage = settings.welcomeMessage;
-    welcomeMessage = welcomeMessage.replace("{{user}}", member.user.tag);
-    welcomeMessage = welcomeMessage.replace("{{guild}}", member.guild.name);
-    welcomeMessage = welcomeMessage.replace("{{memberCount}}", member.guild.memberCount);
-    let welcomeChannel = member.guild.channels.find("name", settings.defaultChannel);
-    if (!welcomeChannel) return member.guild.channels.find(c=> c.permissionsFor(member.guild.me).has("SEND_MESSAGES")).send(`:negative_squared_cross_mark: ${settings.defaultChannel} was not found`);
-    member.guild.channels.find("name", settings.defaultChannel).send(welcomeMessage);
-  }
-  if (settings.autoRole === "true") {
-    let role = member.guild.roles.find("name", settings.autoRole);
-    if (!role) return;
-    member.addRole(role);
-  }
-};
+  bot.on('guildMemberAdd', guildMember => {
+      db.fetchObject(`autoRole_$(guildMember.guild.id)`).then(i => {
+        if (!i.text || i.text.toLowerCase() === 'none') return
+        else {
+          try{
+          }catch (e) {
+            console.log"Auto Role Failed
+  
